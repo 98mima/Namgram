8aefcdf843c75a66c4f54dbd7508b25df820ece6
mila99savic <mila99savic@gmail.com>
2021-01-04 19:42:00 +0100

    dopuna2

--- namgram-api/controllers/comment.js
+++ namgram-api/controllers/comment.js
@@ -15,6 +15,101 @@ function _manyComments(neo4jResult) {
     return neo4jResult.records.map(r => new Comment(r.get('comment')))
 }
 
+exports.getByPost = async (req, res) => {
+    try{
+        let session = driver.session();
+
+        const query = [
+            'MATCH (post:Post {id: $id})<-[r1:commented]-(n:Person) return r1 as comment'
+        ].join('\n')
+
+        return session.readTransaction(txc =>
+            txc.run(query, {
+                id: req.params.postId
+            }))
+            .then( result => {  
+            const listOfComments = _manyComments(result)
+            
+            session.close();
+            const Data = listOfComments
+    
+            res.status(200)
+            .json({ Data })})
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    catch (err){
+        res.json({ success: false });
+        console.log(err)
+    }
+}
+exports.getByImage = async (req, res) => {
+    try{
+        let session = driver.session();
+
+        const query = [
+            'MATCH (image:Image {id: $id})<-[r1:commented]-(n:Person) return r1 as comment'
+        ].join('\n')
+
+        return session.readTransaction(txc =>
+            txc.run(query, {
+                id: req.params.imageId
+            }))
+            .then( result => {  
+            const listOfComments = _manyComments(result)
+            
+            session.close();
+            const Data = listOfComments
+            console.log(req.params.imageId)
+    
+            res.status(200)
+            .json({ Data })})
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    catch (err){
+        res.json({ success: false });
+        console.log(err)
+    }
+}
+exports.addToImage = async (req, res) => {
+    try {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        today = dd + '.' + mm + '.' + yyyy;
+
+        let session = driver.session();
+        const query = [
+            'match (p:Image), (a:Person) \
+             where p.id = $imageId and a.id = $personId\
+             create (p)<-[comment:commented {date:$date, content:$content}]-(a) \
+             RETURN comment'
+        ].join('\n')
+
+        const com = await session.writeTransaction(txc =>
+            txc.run(query, {
+                date: today,
+                content: req.body.content,
+                personId: req.body.personId,
+                imageId: req.body.imageId
+            }))
+
+        const comment = _manyComments(com)
+        const Data = comment[0]
+
+        session.close();
+        res.status(200)
+            .json({ message: "Kreiran komentar", Data })
+    }
+    catch (err) {
+        res.json({ success: false });
+        console.log(err);
+    }
+};
 exports.add = async (req, res) => {
     try {
         var today = new Date();
@@ -52,32 +147,3 @@ exports.add = async (req, res) => {
     }
 };
 
-exports.getByPost = async (req, res) => {
-    try{
-        let session = driver.session();
-
-        const query = [
-            'MATCH (post:Post {id: $id})<-[r1:commented]-(n:Person) return r1 as comment'
-        ].join('\n')
-
-        return session.readTransaction(txc =>
-            txc.run(query, {
-                id: req.params.postId
-            }))
-            .then( result => {  
-            const listOfComments = _manyComments(result)
-            
-            session.close();
-            const Data = listOfComments
-    
-            res.status(200)
-            .json({ message: "Kreiran komentar", Data })})
-            .catch(err => {
-                console.log(err)
-            })
-    }
-    catch (err){
-        res.json({ success: false });
-        console.log(err)
-    }
-}
--- namgram-api/routes/comment.js
+++ namgram-api/routes/comment.js
@@ -6,7 +6,9 @@ const commentController = require('../controllers/comment');
 
 const router = express.Router();
 
-router.post('/add', commentController.add);
 router.get('/byPostId/:postId', commentController.getByPost);
+router.get('/byImageId/:imageId', commentController.getByImage);
+router.post('/add', commentController.add);
+router.post('/addToImage', commentController.addToImage);
 
 module.exports = router;

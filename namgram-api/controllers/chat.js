1c29390523e699441618e040de60272339de9afd
mila99savic <mila99savic@gmail.com>
2021-01-07 14:03:49 +0100

    prikaz followera koji su aktivni

--- namgram-api/controllers/chat.js
+++ namgram-api/controllers/chat.js
@@ -8,10 +8,19 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+const Person = require('../models/person');
+let { creds } = require("./../config/credentials");
+let neo4j = require('neo4j-driver');
+const _ = require('lodash');
+let driver = neo4j.driver("bolt://0.0.0.0:7687", neo4j.auth.basic(creds.neo4jusername, creds.neo4jpw));
 
 var chatters = []
 var chat_messages = []
 var mess = []
+
+function _manyPeople(neo4jResult) {
+    return neo4jResult.records.map(r => new Person(r.get('person')))
+  }
 
 client.once('ready', function () {
     //flush Redis
@@ -56,12 +65,21 @@ exports.getMessages = async (req, res) => {
 
 exports.getActiveChatters = async (req, res) => {
     try {
-        const number = chatters.length
-        res.json({
-            "active": chatters,
-            "status": "OK",
-            number
+        //const number = chatters.length
+        let Data = []
+        const key = JSON.stringify(Object.assign({}, { user: req.params.username }, { collection: "chatters" }));
+        client.get(key, function (err, reply) {
+            if (reply) {
+                Data = JSON.parse(reply)
+                return res.status(200).json(Data)
+            }
+            console.log(err)
         })
+        // res.json({
+        //     "active": chatters,
+        //     "status": "OK",
+        //     number
+        // })
     }
     catch (err) {
         res.json({ success: false });
@@ -71,6 +89,29 @@ exports.getActiveChatters = async (req, res) => {
 
 exports.joinChat = async (req, res) => {
     try {
+        let session = driver.session();
+        const persons = await session.run('MATCH (n:Person {username: $username})<--(person) RETURN person', {
+            username: req.body.username
+        })
+        session.close();
+        const followers = _manyPeople(persons)
+       
+        followers.forEach(follower => {
+            let active = []
+            const keyActive = JSON.stringify(Object.assign({}, { user: follower.username }, { collection: "chatters" }));
+            client.get(keyActive, function (err, reply) {
+                if (reply) {
+                    active = JSON.parse(reply)
+                }
+            })
+            console.log(active)
+            if (active.indexOf(follower.username) == -1) {
+                active.push(req.body.username)
+                client.set(keyActive, JSON.stringify(active))
+            }
+        });
+
+        //prikaz poruka
         var username = req.body.username
         var username2 = req.body.username2
         const users = []
@@ -78,16 +119,12 @@ exports.joinChat = async (req, res) => {
         users.push(username2)
         users.sort()
 
-        if (chatters.indexOf(username) == -1) {
-            chatters.push(username)
-            client.set('active_users', JSON.stringify(chatters))
-        }
         let Data = []
         const key = JSON.stringify(Object.assign({}, { user1: users[0] }, { user2: users[1] }, { collection: "messages" }));
         client.get(key, function (err, reply) {
             if (reply) {
                 Data = JSON.parse(reply)
-                return res.status(200).json({Data, "active": chatters,
+                return res.status(200).json({Data, "active": "nadji aktivne",
                 "status": "OK"})
             }
         })
@@ -100,9 +137,24 @@ exports.joinChat = async (req, res) => {
 
 exports.leaveChat = async (req, res) => {
     try {
-        var username = req.body.username
-        chatters.splice(chatters.indexOf(username), 1)
-        client.set('active_users', JSON.stringify(chatters))
+        let session = driver.session();
+        const persons = await session.run('MATCH (n:Person {username: $username})<--(person) RETURN person', {
+            username: req.body.username
+        })
+        session.close();
+        const followers = _manyPeople(persons)
+
+        followers.forEach(follower => {
+            const keyActive = JSON.stringify(Object.assign({}, { user: follower.username }, { collection: "chatters" }));
+            client.get(keyActive, function (err, reply) {
+                if (reply) {
+                    active = JSON.parse(reply)
+                }
+            })
+            active.splice(active.indexOf(req.body.username), 1)
+            client.set(keyActive, JSON.stringify(active))
+        });
+        
         res.json({ "status": "OK" })
     }
     catch (err) {

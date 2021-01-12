338f739177f28aa1fd0057473a8491c463b06364
zlatkovnik@gmail.com <zlatkovnik@gmail.com>
2021-01-12 20:26:16 +0100

    Chat reducer

--- namgram-react/src/redux/chat/actions.ts
+++ namgram-react/src/redux/chat/actions.ts
@@ -1,3 +1,4 @@
+import { IMessage } from "../../models/chat";
 import { IUser } from "../../models/user";
 import { getFollowers } from "../../services/profile";
 import { SET_ERROR, START_LOADING, STOP_LOADING } from "../ui/actions";
@@ -5,6 +6,9 @@ import { SET_ERROR, START_LOADING, STOP_LOADING } from "../ui/actions";
 
 export const SET_CHAT_HEADS = "SET_CHAT_HEADS"
 export const CLEAR_CHAT_HEADS = "CLEAR_CHAT_HEADS"
+
+export const NEW_MESSAGE = "NEW_MESSAGE"
+export const CLEAR_NEW_MESSAGES = "CLEAR_NEW_MESSAGES"
 
 export interface SetChatHeadsActions {
     type: typeof SET_CHAT_HEADS,
@@ -15,7 +19,16 @@ export interface ClearChatHeads {
     type: typeof CLEAR_CHAT_HEADS
 }
 
-export type ChatActionTypes = SetChatHeadsActions | ClearChatHeads
+export interface NewMessageAction {
+    type: typeof NEW_MESSAGE,
+    payload: IMessage
+}
+
+export interface ClearNewMessages {
+    type: typeof CLEAR_NEW_MESSAGES
+}
+
+export type ChatActionTypes = SetChatHeadsActions | ClearChatHeads | NewMessageAction | ClearNewMessages
 
 export const loadChatHeads = (username: string) => (dispatch: any) => {
     dispatch({type: START_LOADING});
--- namgram-react/src/redux/chat/reducer.ts
+++ namgram-react/src/redux/chat/reducer.ts
@@ -1,14 +1,19 @@
 import { IAuth } from "../../models/auth"
+import { IChat, IMessage } from "../../models/chat"
 import { IUser } from "../../models/user"
-import { ChatActionTypes, SET_CHAT_HEADS, CLEAR_CHAT_HEADS } from "./actions"
+import { ChatActionTypes, SET_CHAT_HEADS, CLEAR_CHAT_HEADS, NEW_MESSAGE, CLEAR_NEW_MESSAGES } from "./actions"
 
 
 export interface ChatState{
-    chatHeads: IUser[]
+    chatHeads: IUser[],
+    chat: IChat | undefined,
+    chatNotifications: number
 }
 
 const initialState: ChatState = {
-    chatHeads: []
+    chatHeads: [],
+    chat: undefined,
+     chatNotifications: 0
 }
 
 export default (state = initialState, action: ChatActionTypes) => {
@@ -17,6 +22,12 @@ export default (state = initialState, action: ChatActionTypes) => {
             return {...state, chatHeads: action.payload}
         case CLEAR_CHAT_HEADS:
             return {...state, chatHeads: []}
+        case NEW_MESSAGE:
+            return {...state, chatNotifications: state.chatNotifications + 1, 
+                chat: {chatter: (state.chat as IChat).chatter, 
+                    messages: [...(state.chat as IChat).messages, action.payload]}}
+        case CLEAR_NEW_MESSAGES:
+            return {...state, chatNotifications: 0}
         default:
             return {...state}
     }

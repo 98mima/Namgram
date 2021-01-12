2325351e22ba301641ed57a47310dab6fe007208
98mima <98mima@gmail.com>
2021-01-12 20:17:28 +0100

    profilna slika

--- namgram-api/controllers/person.js
+++ namgram-api/controllers/person.js
@@ -1,4 +1,5 @@
 const Person = require('../models/person');
+const Image = require('../models/image');
 const controller = require('../controllers/person');
 let { creds } = require("./../config/credentials");
 let neo4j = require('neo4j-driver');
@@ -8,20 +9,42 @@ const redis = require('redis');
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 
+var storage = require("@azure/storage-blob")
+const accountname = "namgram";
+const key = "b9/PjmImjnORF1berLyRe3OYyAO0dDGcTbqIYm5AkCm8tqYukKm/umiUPWLJujc2n+zPFwKbKKNFZAZm8kqWhA==";
+const cerds = new storage.StorageSharedKeyCredential(accountname, key);
+const {
+    BlobServiceClient,
+    StorageSharedKeyCredential,
+    newPipeline
+} = require('@azure/storage-blob');
+const sharedKeyCredential = new StorageSharedKeyCredential(
+    process.env.AZURE_STORAGE_ACCOUNT_NAME,
+    process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY);
+const pipeline = newPipeline(sharedKeyCredential);
+const blobServiceClient = new BlobServiceClient(
+    `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
+    pipeline
+);
+const containerName = 'namgram1609522522970';
+const clientBlob = blobServiceClient.getContainerClient(containerName)
 
 function _manyPeople(neo4jResult) {
     return neo4jResult.records.map(r => new Person(r.get('person')))
-  }
+}
+function _manyimage(neo4jResult) {
+    return neo4jResult.records.map(r => new Image(r.get('image')))
+}
 
-exports.getAll = async (req, res) =>  {
-    try{
+exports.getAll = async (req, res) => {
+    try {
         let session = driver.session();
         const persons = await session.run('MATCH (person:Person) RETURN person', {
         });
         session.close();
         const Data = _manyPeople(persons)
-       res.status(200)
-       .json({message: "Prikupljeno", Data})   
+        res.status(200)
+            .json({ message: "Prikupljeno", Data })
     }
     catch (err) {
         res.json({ success: false });
@@ -29,16 +52,39 @@ exports.getAll = async (req, res) =>  {
     }
 };
 
-exports.get = async (req, res) =>  {
-    try{
+exports.get = async (req, res) => {
+    try {
         let session = driver.session();
         const person = await session.run('MATCH (n:Person {id: $id}) RETURN n', {
             id: req.params.id
         });
-        session.close();
+        const image = await session.run('MATCH (n:Person {id: $id})-[r:created]->(image:Image {person:$id}) RETURN image', {
+            id: req.params.id
+        })
+        const pic = _manyimage(image)
+        console.log(pic)
         const Data = person.records[0].get('n').properties;
-       res.status(200)
-       .json({message: "Prikupljeno", Data})   
+
+        const blobName = pic[0].blobName
+        const blobClient = clientBlob.getBlobClient(blobName);
+        const blobSAS = storage.generateBlobSASQueryParameters({
+            containerName,
+            blobName: blobName,
+            permissions: storage.BlobSASPermissions.parse("racwd"),
+            startsOn: new Date(),
+            expiresOn: new Date(new Date().valueOf() + 86400)
+        },
+            cerds
+        ).toString();
+
+        const sasUrl = blobClient.url + "?" + blobSAS;
+        Data.sasToken = sasUrl
+
+
+
+        session.close();
+        res.status(200)
+            .json({ message: "Prikupljeno", Data })
     }
     catch (err) {
         res.json({ success: false });
@@ -46,16 +92,16 @@ exports.get = async (req, res) =>  {
     }
 };
 
-exports.getByEmail = async (req, res) =>  {
-    try{
+exports.getByEmail = async (req, res) => {
+    try {
         let session = driver.session();
         const person = await session.run('MATCH (n:Person {email: $email}) RETURN n', {
             email: req.params.email
-          })
+        })
         session.close();
         const Data = person.records[0].get('n').properties
         res.status(200)
-            .json({message: "Prikupljeno", Data})
+            .json({ message: "Prikupljeno", Data })
     }
     catch (err) {
         res.json({ success: false });
@@ -63,16 +109,16 @@ exports.getByEmail = async (req, res) =>  {
     }
 };
 
-exports.getByUsername = async (req, res) =>  {
-    try{
+exports.getByUsername = async (req, res) => {
+    try {
         let session = driver.session();
         const person = await session.run('MATCH (n:Person {username: $username}) RETURN n', {
             username: req.params.username
-          })
+        })
         session.close();
         const Data = person.records[0].get('n').properties
         res.status(200)
-            .json({message: "Prikupljeno", Data})
+            .json({ message: "Prikupljeno", Data })
     }
     catch (err) {
         res.json({ success: false });
@@ -80,16 +126,16 @@ exports.getByUsername = async (req, res) =>  {
     }
 };
 
-exports.getFollowing = async (req, res) =>  {
-    try{
+exports.getFollowing = async (req, res) => {
+    try {
         let session = driver.session();
         const persons = await session.run('MATCH (n:Person {username: $username})-->(person) RETURN person', {
             username: req.params.username
-          })
+        })
         session.close();
         const Data = _manyPeople(persons)
         res.status(200)
-            .json({message: "Prikupljeno", Data})
+            .json({ message: "Prikupljeno", Data })
     }
     catch (err) {
         res.json({ success: false });
@@ -97,16 +143,16 @@ exports.getFollowing = async (req, res) =>  {
     }
 };
 
-exports.getFollowers = async (req, res) =>  {
-    try{
+exports.getFollowers = async (req, res) => {
+    try {
         let session = driver.session();
         const persons = await session.run('MATCH (n:Person {username: $username})<--(person) RETURN person', {
             username: req.params.username
-          })
+        })
         session.close();
         const Data = _manyPeople(persons)
         res.status(200)
-            .json({message: "Prikupljeno", Data})
+            .json({ message: "Prikupljeno", Data })
     }
     catch (err) {
         res.json({ success: false });
@@ -114,16 +160,16 @@ exports.getFollowers = async (req, res) =>  {
     }
 };
 
-exports.follow = async (req, res) =>  {
-    try{
+exports.follow = async (req, res) => {
+    try {
         let session = driver.session();
         const rel = await session.run('match (a:Person {username:$username1}),(b:Person {username:$username2}) merge (a)-[r:follows]->(b) return r ', {
             username1: req.body.username1,
             username2: req.body.username2
-          })
+        })
         session.close();
         res.status(200)
-            .json({message: "Follow postavljen", rel})
+            .json({ message: "Follow postavljen", rel })
     }
     catch (err) {
         res.json({ success: false });
@@ -131,16 +177,16 @@ exports.follow = async (req, res) =>  {
     }
 };
 
-exports.unfollow = async (req, res) =>  {
-    try{
+exports.unfollow = async (req, res) => {
+    try {
         let session = driver.session();
         const rel = await session.run('match (a:Person {username:$username1})-[r:follows]->(b:Person {username:$username2}) delete r ', {
             username1: req.body.username1,
             username2: req.body.username2
-          })
+        })
         session.close();
         res.status(200)
-            .json({message: "Follow izbrisan", rel})
+            .json({ message: "Follow izbrisan", rel })
     }
     catch (err) {
         res.json({ success: false });
@@ -148,13 +194,14 @@ exports.unfollow = async (req, res) =>  {
     }
 };
 
-exports.deletePerson = async  (req, res) => {
+exports.deletePerson = async (req, res) => {
     let session = driver.session();
     try {
         user = await session.run('MATCH (n:Person {username: $id}) DELETE n', {
-            id: req.body.username});
+            id: req.body.username
+        });
         res.status(200)
-            .json({message: "Obrisan", user});
+            .json({ message: "Obrisan", user });
     }
     catch (err) {
         res.json({ success: false });

b83daf347b5b2d264999276c12d76d21582aa596
Aleksa404 <aleksantic1@gmail.com>
2021-01-14 20:35:11 +0100

    izmene

--- namgram-react/src/components/navigation/Navbar.tsx
+++ namgram-react/src/components/navigation/Navbar.tsx
@@ -1,38 +1,43 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import { Avatar, Button, createStyles, fade, makeStyles, Theme } from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import { useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../redux';
-import { logoutAction } from '../../redux/auth/actions';
-import { AddCircleRounded } from '@material-ui/icons';
-
-
+import {
+  Avatar,
+  Button,
+  createStyles,
+  fade,
+  makeStyles,
+  Theme,
+} from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux";
+import { logoutAction } from "../../redux/auth/actions";
+import { AddCircleRounded } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     link: {
       textDecoration: "none",
-      color: "inherit"
+      color: "inherit",
     },
-    btn:{
-      color: 'white',
-      paddingRight: '1rem'
+    btn: {
+      color: "white",
+      paddingRight: "1rem",
     },
     grow: {
       flexGrow: 1,
@@ -41,58 +46,58 @@ const useStyles = makeStyles((theme: Theme) =>
       marginRight: theme.spacing(2),
     },
     title: {
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block",
       },
     },
     search: {
-      position: 'relative',
+      position: "relative",
       borderRadius: theme.shape.borderRadius,
       backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
+      "&:hover": {
         backgroundColor: fade(theme.palette.common.white, 0.25),
       },
       marginRight: theme.spacing(2),
       marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
         marginLeft: theme.spacing(3),
-        width: 'auto',
+        width: "auto",
       },
     },
     searchIcon: {
       padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
     },
     inputRoot: {
-      color: 'inherit',
+      color: "inherit",
     },
     inputInput: {
       padding: theme.spacing(1, 1, 1, 0),
       // vertical padding + font size from searchIcon
       paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: '20ch',
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: "20ch",
       },
     },
     sectionDesktop: {
-      display: 'none',
-      [theme.breakpoints.up('md')]: {
-        display: 'flex',
+      display: "none",
+      [theme.breakpoints.up("md")]: {
+        display: "flex",
       },
     },
     sectionMobile: {
-      display: 'flex',
-      [theme.breakpoints.up('md')]: {
-        display: 'none',
+      display: "flex",
+      [theme.breakpoints.up("md")]: {
+        display: "none",
       },
     },
     logo: {
@@ -102,69 +107,73 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "70px",
       "&:hover": {
         background: "rgb(220, 220, 220, 50)",
-        cursor: "pointer"
-      }
-    }
-  }),
+        cursor: "pointer",
+      },
+    },
+  })
 );
 
 function Navbar() {
-    const history = useHistory();
-    const dispatch = useDispatch();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    const auth = useSelector((state: RootState) => state.auth.auth);
-    const socket = useSelector((state: RootState) => state.auth.socket);
-    const notifications = useSelector((state: RootState) => state.auth.notifications);
+  const auth = useSelector((state: RootState) => state.auth.auth);
+  const socket = useSelector((state: RootState) => state.auth.socket);
+  const notifications = useSelector(
+    (state: RootState) => state.auth.notifications
+  );
 
-    const classes = useStyles();
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [
+    mobileMoreAnchorEl,
+    setMobileMoreAnchorEl,
+  ] = React.useState<null | HTMLElement>(null);
 
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-    const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null);
-    };
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
 
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-        handleMobileMenuClose();
-    };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
 
-    const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-        setMobileMoreAnchorEl(event.currentTarget);
-    };
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
 
-    const handleMyProfile = () => {
-      history.push('/profile/' + auth?.id as string);
-      handleMenuClose();
-    }
+  const handleMyProfile = () => {
+    history.push(("/profile/" + auth?.id) as string);
+    handleMenuClose();
+  };
 
-    const handleLogout = () => {
-      dispatch(logoutAction());
-      handleMenuClose();
-    }
+  const handleLogout = () => {
+    dispatch(logoutAction());
+    handleMenuClose();
+  };
 
-    const handleHome = () => {
-      if(auth) history.push("/posts");
-      else history.push("/");
-    }
+  const handleHome = () => {
+    if (auth) history.push("/posts");
+    else history.push("/");
+  };
 
-
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
 
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
@@ -173,14 +182,14 @@ function Navbar() {
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={mobileMenuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
@@ -213,21 +222,23 @@ function Navbar() {
       </MenuItem>
     </Menu>
   );
-    return (
-        <div className={classes.grow}>
+  return (
+    <div className={classes.grow}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
+          {/* <IconButton
             edge="start"
             className={classes.menuButton}
             color="inherit"
             aria-label="open drawer"
           >
-            <MenuIcon />
-          </IconButton>
+            <MenuIcon /> 
+          </IconButton> */}
           <Link to="/" className={classes.link}>
-          <img className={classes.logo} 
-          src="https://cdn.discordapp.com/attachments/777890574253817889/792441180054749224/e52d18ae-4e0c-40cf-8d30-07396304f4e0_200x200.png" />
+            <img
+              className={classes.logo}
+              src="https://cdn.discordapp.com/attachments/777890574253817889/792441180054749224/e52d18ae-4e0c-40cf-8d30-07396304f4e0_200x200.png"
+            />
           </Link>
           {/* <Typography className={classes.title} variant="h6" noWrap>
             namgram
@@ -242,49 +253,60 @@ function Navbar() {
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
             />
           </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
-            {!auth ? 
-            <div>
-              <Link to="/signin" style={{textDecoration: 'none'}}>
-                <Button className={classes.btn} variant='outlined' color='primary'>Sign in</Button>
-              </Link>
-              <Link to="/signup" style={{textDecoration: 'none'}}>
-                <Button variant='contained' color='secondary'>Sign up</Button>
-              </Link>
-            </div> : 
-            <div>
-              <Link className={classes.link} to="/posts/create">
-              <IconButton aria-label="Add new image" color="inherit">
-                <AddCircleRounded />
-              </IconButton>
-              </Link>
-              <Link className={classes.link} to="/chat">
-               <IconButton aria-label="Chat" color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            </Link>
-            <IconButton aria-label="show new notifications" color="inherit">
-              <Badge badgeContent={notifications.length} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-            edge="end"
-            aria-label="account of current user"
-            aria-controls={menuId}
-            aria-haspopup="true"
-            onClick={handleProfileMenuOpen}
-            color="inherit"
-          >
-            {/* <AccountCircle /> */}
-            <Avatar src={auth.profilePic} />
-          </IconButton> </div>}
+            {!auth ? (
+              <div>
+                <Link to="/signin" style={{ textDecoration: "none" }}>
+                  <Button
+                    className={classes.btn}
+                    variant="outlined"
+                    color="primary"
+                  >
+                    Sign in
+                  </Button>
+                </Link>
+                <Link to="/signup" style={{ textDecoration: "none" }}>
+                  <Button variant="contained" color="secondary">
+                    Sign up
+                  </Button>
+                </Link>
+              </div>
+            ) : (
+              <div>
+                <Link className={classes.link} to="/posts/create">
+                  <IconButton aria-label="Add new image" color="inherit">
+                    <AddCircleRounded />
+                  </IconButton>
+                </Link>
+                <Link className={classes.link} to="/chat">
+                  <IconButton aria-label="Chat" color="inherit">
+                    <Badge badgeContent={4} color="secondary">
+                      <MailIcon />
+                    </Badge>
+                  </IconButton>
+                </Link>
+                <IconButton aria-label="show new notifications" color="inherit">
+                  <Badge badgeContent={notifications.length} color="secondary">
+                    <NotificationsIcon />
+                  </Badge>
+                </IconButton>
+                <IconButton
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="inherit"
+                >
+                  {/* <AccountCircle /> */}
+                  <Avatar src={auth.profilePic} />
+                </IconButton>{" "}
+              </div>
+            )}
           </div>
           <div className={classes.sectionMobile}>
             <IconButton
@@ -302,7 +324,7 @@ function Navbar() {
       {renderMobileMenu}
       {renderMenu}
     </div>
-    )
+  );
 }
 
-export default Navbar
+export default Navbar;

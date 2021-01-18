d986c74631e1412b79d03337121decbfe95f485c
Aleksa404 <aleksantic1@gmail.com>
2021-01-18 17:26:45 +0100

    recommended

--- namgram-react/src/components/posts/Posts.tsx
+++ namgram-react/src/components/posts/Posts.tsx
@@ -1,42 +1,137 @@
-import { CircularProgress, createStyles, makeStyles, Theme } from '@material-ui/core';
-import React, { useEffect } from 'react'
+import {
+  Avatar,
+  Button,
+  CircularProgress,
+  Container,
+  createStyles,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+import React, { useEffect } from "react";
 
-import {useSelector, useDispatch} from 'react-redux'
-import { RootState } from '../../redux';
-import { loadPosts } from '../../redux/posts/actions';
-import Post from './Post';
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { IUser } from "../../models/user";
+import { RootState } from "../../redux";
+import { loadPosts } from "../../redux/posts/actions";
+import { getRecommended } from "../../services/profile";
+import Post from "./Post";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-    container:{
-        display: "flex",
-        flexDirection: "column"
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      position: "absolute",
+      justifyContent: "center",
     },
     loading: {
-        margin: "20vh auto auto auto"
-    }
-}));
+      margin: "20vh auto auto auto",
+    },
+    button: {
+      textDecoration: "none",
+    },
+    root: {
+      width: "500px",
+      maxWidth: 360,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: theme.palette.background.paper,
+    },
+    bigContainer: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+    },
+    suggestions: {
+      display: "flex",
+      flexDirection: "column",
+      position: "absolute",
+      alignSelf: "flex-start",
+      left: "2rem",
+      top: "5rem",
+    },
+    hidden: {
+      display: "none",
+    },
+  })
+);
 
 function Posts() {
-    const classes= useStyles();
-    const dispatch = useDispatch();
-    const posts = useSelector((state: RootState) => state.posts.posts);
-    const auth = useSelector((state: RootState) => state.auth.auth);
-    const socket = useSelector((state: RootState) => state.auth.socket)
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const posts = useSelector((state: RootState) => state.posts.posts);
+  const auth = useSelector((state: RootState) => state.auth.auth);
+  const socket = useSelector((state: RootState) => state.auth.socket);
+  const history = useHistory();
+  const matches = useMediaQuery("(min-width:800px)");
 
-    useEffect(() => {
-        if(auth) dispatch(loadPosts(auth?.id as string));
-    }, [auth]);
+  const [profiles, setProfiles] = React.useState<IUser[]>([]);
 
-    const loading = useSelector((state: RootState) => state.ui.loading);    
+  useEffect(() => {
+    if (auth) dispatch(loadPosts(auth?.id as string));
+    if (profiles.length == 0) {
+      getRecommended(auth?.username as string).then((res) => {
+        setProfiles(res);
+      });
+    }
+  }, [auth, profiles]);
 
-    return (
-        <React.Fragment>
-            <div className={classes.container}>
-                {loading && <CircularProgress size={'20vw'} className={classes.loading} />}
-                {posts && posts.map(post => <Post key={post.id} socket={socket as SocketIOClient.Socket} post={post} />)}
-            </div>
-        </React.Fragment>
-    )
+  const loading = useSelector((state: RootState) => state.ui.loading);
+
+  const visit = (id: string) => {
+    history.push(`/profile/${id}`);
+  };
+
+  return (
+    <div className={classes.bigContainer}>
+      <Container className={matches ? classes.suggestions : classes.hidden}>
+        <Typography style={{ color: "white" }}>Suggestions For You</Typography>
+        <List className={classes.root}>
+          {profiles.map((person) => (
+            <ListItem key={person.id}>
+              <ListItemAvatar>
+                <Avatar src={person.profilePic}></Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={person.username}
+                secondary={person.name + " " + person.lastname}
+              />
+
+              <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                onClick={() => visit(person.id)}
+              >
+                Visit
+              </Button>
+            </ListItem>
+          ))}
+        </List>
+      </Container>
+      <div className={classes.container}>
+        {loading && (
+          <CircularProgress size={"20vw"} className={classes.loading} />
+        )}
+        {posts &&
+          posts.map((post) => (
+            <Post
+              key={post.id}
+              socket={socket as SocketIOClient.Socket}
+              post={post}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
 
-export default Posts
+export default Posts;
--- namgram-react/src/services/profile.ts
+++ namgram-react/src/services/profile.ts
@@ -5,7 +5,14 @@ export async function getProfile(userId: string) {
   return axios
     .get<{ message: string; Data: IUser }>(`person/byId/${userId}`)
     .then((res) => {
-      const { id, birthday, lastname, name, username, profilePic } = res.data.Data;
+      const {
+        id,
+        birthday,
+        lastname,
+        name,
+        username,
+        profilePic,
+      } = res.data.Data;
       const profile: IProfile = {
         id,
         birthday,
@@ -15,14 +22,16 @@ export async function getProfile(userId: string) {
         followers: [],
         following: [],
         posts: [],
-        profilePic
+        profilePic,
       };
       return profile;
     });
 }
 
-export async function getProfileByUsername(username: string){
-  return axios.get<{message: string, Data: IUser}>(`person/byUsername/${username}`).then(res => res.data.Data)
+export async function getProfileByUsername(username: string) {
+  return axios
+    .get<{ message: string; Data: IUser }>(`person/byUsername/${username}`)
+    .then((res) => res.data.Data);
 }
 
 export async function getFollowers(username: string) {
@@ -58,5 +67,15 @@ export async function unfollow(follower: string, followee: string) {
     })
     .then((res) => {
       return res.data;
+    });
+}
+
+export async function getRecommended(username: string) {
+  return axios
+    .get<{ message: string; Data: IUser[] }>(
+      `person/getRecommendedPeople/${username}`
+    )
+    .then((res) => {
+      return res.data.Data;
     });
 }

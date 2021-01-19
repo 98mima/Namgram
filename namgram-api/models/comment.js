08ed65722425e378c3409f0e117f7643510be49d
98mima <98mima@gmail.com>
2021-01-19 21:25:45 +0100

    komentari reseni (sa brisanjem)

--- namgram-api/models/comment.js
+++ namgram-api/models/comment.js
@@ -5,6 +5,7 @@ let driver = neo4j.driver("bolt://0.0.0.0:7687", neo4j.auth.basic(creds.neo4juse
 
 const Post = module.exports = function (_node) {
     _.extend(this, {
+        'commId': _node.properties['commId'],
         'date': _node.properties['date'],
         'content': _node.properties['content'],
         'creator': _node.properties['creator'],

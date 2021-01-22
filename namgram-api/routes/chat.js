7a988a7ccf94a01295f2f273dcf704fec1fd06ff
98mima <98mima@gmail.com>
2021-01-22 20:40:59 +0100

    aktivni useri

--- namgram-api/routes/chat.js
+++ namgram-api/routes/chat.js
@@ -5,6 +5,7 @@ const chatController = require('../controllers/chat');
 const router = express.Router();
 
 router.get('/getMessages/:username1/:username2', chatController.getMessages);
+router.get('/getActive/:username', chatController.getActive);
 router.post('/join', chatController.joinChat);
 router.post('/leave', chatController.leaveChat);
 router.post('/send', chatController.sendMessage);
